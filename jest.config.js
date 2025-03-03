b518aa3da061ffe20ec4d7e29edfbf16bfc6686e
mnguyencfcu <mnguyen@congressionalfcu.org>
2025-03-03 07:20:41 -0500

    update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,24 +1,12 @@
 module.exports = {
-  collectCoverage: true,
-  coverageReporters: ["lcov", "text"],
+    collectCoverage: true,
     coverageDirectory: "coverage",
-    // Normalize paths for Windows environments
-    reporters: [
-        "default",
-        [
-            "jest-junit",
-            {
-                outputDirectory: "coverage",
-                outputName: "junit.xml",
-            },
-        ],
-    ],
+    coverageReporters: ["lcov", "text"],
+    testResultsProcessor: "./normalizePaths.js",  // Custom script for path normalization
+
     transform: {
         "^.+\\.js$": "babel-jest",
     },
     modulePathIgnorePatterns: ["<rootDir>/node_modules/"],
     coveragePathIgnorePatterns: ["<rootDir>/node_modules/"],
-
-    // Custom fix: Normalize file paths to use forward slashes
-    testResultsProcessor: "./normalizePaths.js",
 };
