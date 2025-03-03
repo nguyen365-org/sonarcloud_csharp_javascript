27017e490625413db79cb3da7a7d282d801cfe49
mnguyencfcu <mnguyen@congressionalfcu.org>
2025-03-03 07:35:10 -0500

    change slashes

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,5 @@
 module.exports = {
     collectCoverage: true,
     coverageDirectory: "coverage",
-    coverageReporters: ["lcov", "text"],
-    testResultsProcessor: "./normalizePaths.js",  // Custom script for path normalization
-
-    transform: {
-        "^.+\\.js$": "babel-jest",
-    },
-    modulePathIgnorePatterns: ["<rootDir>/node_modules/"],
-    coveragePathIgnorePatterns: ["<rootDir>/node_modules/"],
+    coverageReporters: ["lcov", "text"]
 };
