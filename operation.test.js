fd30d5e52394575913a9d5d85fa1eb75a5c630a1
Minh Tuyen Nguyen <minh@nguyen365.com>
2025-03-05 18:56:23 -0500

    js at root

--- operation.test.js
+++ operation.test.js
@@ -1,4 +1,4 @@
-﻿const { add, subtract } = require('../Scripts/operation');
+﻿const { add, subtract } = require('./operation');
 
 test('adds 1 + 2 to equal 3', () => {
     expect(add(1, 2)).toBe(3);
