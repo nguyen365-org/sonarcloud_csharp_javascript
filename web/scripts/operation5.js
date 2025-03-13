f8897e8c546f64e4eec25a8d0efc04818ad2d7d5
Minh Tuyen Nguyen <minh@nguyen365.com>
2025-03-13 16:32:33 -0400

    one word lower case project

---
+++ web/scripts/operation5.js
@@ -0,0 +1,5 @@
+﻿function increment(a) {
+    a = a + 1;
+    return a; 
+}
+module.exports = { increment };
---
+++ web/tests/operation5.test.js
@@ -0,0 +1,5 @@
+﻿const { increment } = require('../scripts/operation5');
+
+test('2 increment to equal 3', () => {
+    expect(increment(2)).toBe(3);
+});
