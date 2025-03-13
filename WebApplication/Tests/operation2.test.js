672e07dcd1ec6808f4bb9a54601e622cc75f64e7
Minh Tuyen Nguyen <minh@nguyen365.com>
2025-03-13 15:02:06 -0400

    Added javascript file inside project.

---
+++ WebApplication/Tests/operation2.test.js
@@ -0,0 +1,10 @@
+﻿const { multiply, divide } = require('../Scripts/operation2');
+
+test('adds 1 + 2 to equal 2', () => {
+    expect(multiply(1, 2)).toBe(2);
+
+});
+test('4 / 2 to equal 2', () => {
+    expect(divide(4, 2)).toBe(2);
+
+});
