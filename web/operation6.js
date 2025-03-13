12f13871fb6654186c130dffdd57492d957be955
Minh Tuyen Nguyen <minh@nguyen365.com>
2025-03-13 16:42:49 -0400

    Added js.

---
+++ web/operation6.js
@@ -0,0 +1,5 @@
+﻿function decrement(a) {
+    a = a - 1;
+    return a; 
+}
+module.exports = { decrement };
---
+++ web/operation6.test.js
@@ -0,0 +1,5 @@
+﻿const { decrement } = require('./operation6');
+
+test('2 decrement to equal 1', () => {
+    expect(decrement(2)).toBe(1);
+});
